7b2591d448a35157f4255ffb29d7488514b37cce
AbhiGattineni <abhishekgattineni@gmail.com>
2025-01-25 23:21:54 -0800

    IM-0125 | fixed authentication configuration amplifyconfig

--- js/src/amplifyConfig.js
+++ js/src/amplifyConfig.js
@@ -2,10 +2,12 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    region: process.env.AWS_REGION,
-    userPoolId: process.env.USER_POOL_ID,
-    userPoolWebClientId: process.env.USER_POOL_CLIENT_ID,
-    mandatorySignIn: false, // Optional: Enforce user login before accessing the app
+    Cognito: {
+      region: import.meta.REACT_APP_AWS_REGION,
+      userPoolId: import.meta.REACT_APP_USER_POOL_ID,
+      userPoolWebClientId: import.meta.REACT_APP_USER_POOL_CLIENT_ID,
+      mandatorySignIn: false, // Optional: Enforce user login before accessing the app
+    },
   },
 });
 
--- js/src/index.jsx
+++ js/src/index.jsx
@@ -3,12 +3,15 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/index.css';
-import './amplifyConfig';
+import amplifyConfig from './amplifyConfig.js';
+import { Amplify } from 'aws-amplify';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
   cache: new InMemoryCache(),
 });
+
+Amplify.configure(amplifyConfig);
 
 const root = createRoot(document.getElementById('root'));
 root.render(
