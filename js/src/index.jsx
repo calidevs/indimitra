57ac5fac62545f4d05c3031baee83b619405bec1
AbhiGattineni <abhishekgattineni@gmail.com>
2025-02-10 19:44:38 -0800

    IM-02102025 | React Query Setup

--- js/src/index.jsx
+++ js/src/index.jsx
@@ -1,23 +1,20 @@
 import { createRoot } from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx';
 import './styles/index.css';
 import amplifyConfig from './amplifyConfig.js';
 import { Amplify } from 'aws-amplify';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
-});
-
 Amplify.configure(amplifyConfig);
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ApolloProvider>
+  </QueryClientProvider>
 );
