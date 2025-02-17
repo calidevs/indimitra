1a9615fde271f4fda570997c98eb8242f543c6c8
StephenJakku <uttamjakku96@gmail.com>
2025-02-17 15:18:00 -0800

    update schema to include user entity

--- python/app/graphql/schema.py
+++ python/app/graphql/schema.py
@@ -1,4 +1,14 @@
 import strawberry
-from app.graphql.resolvers.product_resolver import Query, Mutation
+from app.graphql.resolvers.product_resolver import Query as ProductQuery, Mutation as ProductMutation
+from app.graphql.resolvers.user_resolver import Query as UserQuery, Mutation as UserMutation
+
+# TODO: Find a better way to add resolvers to the schema
+@strawberry.type
+class Query(ProductQuery, UserQuery):
+    pass
+
+@strawberry.type
+class Mutation(ProductMutation, UserMutation):
+    pass
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
