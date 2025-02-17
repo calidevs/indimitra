8f214aa607bb0b00394162917a44a70559a355df
StephenJakku <uttamjakku96@gmail.com>
2025-02-17 14:20:00 -0800

    * update config to use sqlalchemy to construct db url instead of a string
    * update bootstrap to not run everytime

--- python/app/config.py
+++ python/app/config.py
@@ -1,3 +1,18 @@
+import os
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
 
-# replace db with localhost if you are spinning up fast api in local instead of docker
-DATABASE_URL = "postgresql://indimitra:indimitra123@db:5432/indimitra"
+# Load environment variables from the .env file (make sure to create .env file in project root)
+load_dotenv()
+
+def get_database_url():
+    return URL.create(
+        drivername="postgresql",
+        username=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        database=os.getenv("POSTGRES_DB"),
+    )
+
+DATABASE_URL = get_database_url()
--- python/dev_bootstrap/bootstrap.py
+++ python/dev_bootstrap/bootstrap.py
@@ -9,11 +9,19 @@ from app.db.session import engine
 def create_data():
     # Create tables if they don’t exist yet
     Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+    
+    # Check if there's already at least one product in the database
+    if db.query(ProductModel).first():
+        print("Data already exists, skipping bootstrap.")
+        return
+    else:
+        print("No data exists in DB, Starting bootstrap.")
 
+    
     with open("dev_bootstrap/product_data.json", "r") as file:
         data = json.load(file)
-
-    db = SessionLocal()
+        
     try:
         products = []
         for item in data:
