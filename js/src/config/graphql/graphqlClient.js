63d9a1e9fa06a53fefab49bd3695bfad263e0ad7
abhishekgattineni <abhishekgattineni@gmail.com>
2025-04-10 21:49:30 +0000

    CART PAGE: separeted cart component onto separate page

--- js/src/config/graphql/graphqlClient.js
+++ js/src/config/graphql/graphqlClient.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const API_URL = 'http://54.81.162.234:8000/graphql';
+const API_URL = 'https://indimitra.com/graphql';
 
 const graphQLClient = new GraphQLClient(API_URL, {
   headers: {
