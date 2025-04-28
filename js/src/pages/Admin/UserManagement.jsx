561a306b694b1e88ce64d495200b5f680cfd5f9b
abhishekgattineni <abhishekgattineni@gmail.com>
2025-04-28 23:09:47 +0000

    DELETEICON | Changed the delete icon, as we cannot delte users form the admin panel now

--- js/src/pages/Admin/UserManagement.jsx
+++ js/src/pages/Admin/UserManagement.jsx
@@ -336,9 +336,6 @@ const UserManagement = () => {
                   <IconButton size="small" color="primary" onClick={() => handleRoleUpdate(user)}>
                     <EditIcon />
                   </IconButton>
-                  <IconButton size="small" color="error">
-                    <DeleteIcon />
-                  </IconButton>
                   {user.type === 'DELIVERY' && (
                     <IconButton
                       size="small"
@@ -390,9 +387,6 @@ const UserManagement = () => {
                   <IconButton size="small" color="primary" onClick={() => handleRoleUpdate(user)}>
                     <EditIcon />
                   </IconButton>
-                  <IconButton size="small" color="error">
-                    <DeleteIcon />
-                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
@@ -437,9 +431,6 @@ const UserManagement = () => {
                     onClick={() => handleRoleUpdate(partner)}
                   >
                     <EditIcon />
-                  </IconButton>
-                  <IconButton size="small" color="error">
-                    <DeleteIcon />
                   </IconButton>
                   <IconButton
                     size="small"
@@ -493,9 +484,6 @@ const UserManagement = () => {
                     onClick={() => handleRoleUpdate(manager)}
                   >
                     <EditIcon />
-                  </IconButton>
-                  <IconButton size="small" color="error">
-                    <DeleteIcon />
                   </IconButton>
                 </TableCell>
               </TableRow>
