99fc0cff020e2219a939380a3d6125aceed8fc89
StephenJakku <uttamjakku96@gmail.com>
2025-05-21 00:30:15 -0700

    alembic migrations

---
+++ python/app/db/migrations/versions/608fb11b9a00_autogen_phase2_bring_back_not_null_.py
@@ -0,0 +1,24 @@
+"""autogen phase2: bring back not null fields
+
+Revision ID: 608fb11b9a00
+Revises: 526d54f3b3dd
+Create Date: 2025-05-21 00:19:07.056081
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '608fb11b9a00'
+down_revision = '526d54f3b3dd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
