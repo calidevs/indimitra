e68e47247fd6ade7e899f9c28ad9aab208f15e40
gundlatrinath <trinatgundla@gmail.com>
2025-06-16 23:47:01 +0530

    added close button for change store dialog

--- js/src/components/Dialog/Dialog.jsx
+++ js/src/components/Dialog/Dialog.jsx
@@ -1,5 +1,6 @@
-import { Dialog as MuiDialog, DialogTitle, DialogContent } from '@mui/material';
+import { Dialog as MuiDialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Dialog = ({ open, onClose, children, title, ...props }) => {
   const theme = useTheme();
@@ -25,9 +26,25 @@ const Dialog = ({ open, onClose, children, title, ...props }) => {
             color: 'white',
             py: 2.5,
             mb: 2,
+            position: 'relative',
           }}
         >
           {title}
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: 'white',
+              '&:hover': {
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+              },
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
         </DialogTitle>
       )}
 
